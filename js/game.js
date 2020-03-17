6d5f0a53b177839f58a1e698f3eae682d693d886
ChristianOtieno <cgao273@gmail.com>
2020-03-18 01:35:54 +0300

    add playing logic for player's inputs and turn

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 const gameBoard = (() => {
 
   const board = () => {
-    return ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+    return ['', '', '', '', '', '', '', '', ''];
   }
 
   return { board }
@@ -11,5 +11,48 @@ const Player = (name) => {
   return { name } 
 };
 
+
+const Xplay = 'X';
+const Oplay = 'O';
+let currentPlayer = Xplay;
+const playBoard =  document.getElementById('board')
+
+// updates the board with players input
+playBoard.addEventListener("click", (element) => {
+  if (element.target.innerText ===  'X' || element.target.innerText === 'O') {
+    return;
+  };
+  element.target.innerText = currentPlayer;
+  currentPlayer = (currentPlayer ===  Xplay ? Oplay : Xplay);
+});
+
+
+// winning permutations
+const winningCombinations = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+];
+
+const checkWins = () => {
+
+};
+
+const checkDraws = () => {
+
+};
+
+const playerTurn = () => {
+
+};
+
+const clearBoard = () => {
+
+};
 
 export { gameBoard, Player };

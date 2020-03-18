aa1e355db5afeb86997efc743f009d6468690044
ChristianOtieno <cgao273@gmail.com>
2020-03-18 20:10:56 +0300

    add style linter and refactor the code

--- js/game.js
+++ js/game.js
@@ -1,74 +1,74 @@
 const gameBoard = (() => {
-
-  const board = () => {
-    return ['', '', '', '', '', '', '', '', ''];
-  }
-
-  const play = (player1Array, player2Array) => {
-    
-    const playerOne = player1Array[0];
-    const Xplay = player1Array[1];
-    const playerTwo = player2Array[0];
-    const Oplay = player2Array[1];
-
-    let currentMove = Xplay
-    let currentPlayer = playerOne;
-    const playBoard =  document.getElementById('board');
-    let winner = '';
-
-
-    // updates the board with players input
-    playBoard.addEventListener("click", function listener(element) {
-      console.log(currentPlayer)
-      //checks if position is ocupied 
-      if (element.target.innerText ===  'X' || element.target.innerText === 'O') {
-        return;
-      };
-      element.target.innerText = currentMove;
-      
-      // checks for winner
-      if (checkWins(currentPlayer, currentMove)[0]) {
-        playBoard.removeEventListener('click', listener);
-        winner = checkWins(currentPlayer, currentMove)[1]
-      }
-      currentPlayer = (currentPlayer ===  playerOne ? playerTwo : playerOne);
-      currentMove = (currentMove ===  Xplay ? Oplay : Xplay);
-    });
-    return winner;
-  }
-
+  const board = () => ['', '', '', '', '', '', '', '', ''];
 
   // winning permutations
   const winningCombinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
 
   const checkWins = (player, move) => {
     let response = false;
-    winningCombinations.map( combo => {
+    winningCombinations.map((combo) => {
       const one = document.getElementById(combo[0]);
       const two = document.getElementById(combo[1]);
       const three = document.getElementById(combo[2]);
 
-      if((one.innerText === move) && (two.innerText === move) && (three.innerText === move)) {
+      if ((one.innerText === move) && (two.innerText === move) && (three.innerText === move)) {
         response = true;
         const playerNames = document.getElementById('player-names');
-        console.log(player)
+        console.log(player);
         playerNames.innerHTML = `${player.name} wins!`;
       }
     });
     return [response, player];
   };
 
-  const checkDraws = () => {
+  const checkDraws = (player, move) => {
+    // for (let i = 0; i < board.length; i++) {
+    //   // if board[i] === undefined && response === false {}
 
+    }
+  }
+
+  const play = (player1Array, player2Array) => {
+    const playerOne = player1Array[0];
+    const Xplay = player1Array[1];
+    const playerTwo = player2Array[0];
+    const Oplay = player2Array[1];
+
+    let currentMove = Xplay;
+    let currentPlayer = playerOne;
+    const playBoard = document.getElementById('board');
+    let winner = '';
+
+    // updates the board with players input
+    playBoard.addEventListener('click', function listener(element) {
+      console.log(currentPlayer);
+      // checks if position is ocupied
+      if (element.target.innerText === 'X' || element.target.innerText === 'O') {
+        return;
+      }
+      element.target.innerText = currentMove;
+
+      // checks for winner
+      if (checkWins(currentPlayer, currentMove)[0]) {
+        playBoard.removeEventListener('click', listener);
+        winner = checkWins(currentPlayer, currentMove)[1];
+      }
+      currentPlayer = (currentPlayer === playerOne ? playerTwo : playerOne);
+      currentMove = (currentMove === Xplay ? Oplay : Xplay);
+    });
+    return winner;
+
+    //checks for draw
+    if ()
   };
 
   const playerTurn = () => {
@@ -80,11 +80,9 @@ const gameBoard = (() => {
   };
 
 
-  return { board, play, checkWins }
+  return { board, play, checkWins };
 })();
 
-const Player = (name) => { 
-  return { name } 
-};
+const Player = (name) => ({ name });
 
 export { gameBoard, Player };
--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-
 import { gameBoard, Player } from './game.js';
 
 const displayController = (() => {
-  
   const display = (board) => {
     const divBoard = document.getElementById('board');
-    board.map( (element, index) => {
-      divBoard.innerHTML += `<div id="${index}">${element}</div>`
+    board.map((element, index) => {
+      divBoard.innerHTML += `<div id="${index}">${element}</div>`;
     });
-  }
+  };
 
   const toggleGame = document.getElementById('toggle-game');
   const input = document.getElementById('input');
@@ -17,26 +15,26 @@ const displayController = (() => {
   const player1Input = document.getElementById('player-1');
   const player2Input = document.getElementById('player-2');
   const playerNames = document.getElementById('player-names');
+  const players = [];
 
   toggleGame.onclick = () => {
     const boardWrap = document.getElementById('board');
     boardWrap.classList.toggle('d-none');
     input.classList.toggle('d-none');
 
-    gameBoard.play([Player(players[0]), 'X'], [Player(players[1]), 'O'])
+    gameBoard.play([Player(players[0]), 'X'], [Player(players[1]), 'O']);
 
-    if (toggleGame.innerHTML === "Start Game") {
-      toggleGame.innerHTML = "Restart Game";
+    if (toggleGame.innerHTML === 'Start Game') {
+      toggleGame.innerHTML = 'Restart Game';
     } else {
-      toggleGame.innerHTML = "Start Game";
+      toggleGame.innerHTML = 'Start Game';
     }
   };
 
-  const players = [];
 
   player1NameToggle.onclick = () => {
     let player1 = document.getElementById('name1').value;
-    if(player1 === '') { player1 = 'Player X' };
+    if (player1 === '') { player1 = 'Player X'; }
     players.push(player1);
     player1Input.classList.toggle('d-none');
     player2Input.classList.toggle('d-none');
@@ -44,7 +42,7 @@ const displayController = (() => {
 
   player2NameToggle.onclick = () => {
     let player2 = document.getElementById('name2').value;
-    if(player2 === '') { player2 = 'Player O' };
+    if (player2 === '') { player2 = 'Player O'; }
     players.push(player2);
     player2Input.classList.toggle('d-none');
     toggleGame.classList.toggle('d-none');
@@ -52,10 +50,7 @@ const displayController = (() => {
       ${players[0]} vs ${players[1]}`;
   };
 
-  return { display }
+  return { display };
 })();
 
-
-
 displayController.display(gameBoard.board());
-

f8a8a27a0965f11bfa2b57b04df3bd296fe1524e
Christian Otieno <oduorchristian@gmail.com>
2020-04-18 00:51:42 +0300

    add tests for board presence

--- tests/game.test.js
+++ tests/game.test.js
@@ -1,5 +1,33 @@
 import { gameBoard } from '../src/dom/game';
 
-test('it works', () => {
-  expect(1 + 2).toEqual(3);
+const newBoard = gameBoard.board();
+
+// tests empty board
+test('it should return an array of empty strings', () => {
+  expect(newBoard).toEqual(['', '', '', '', '', '', '', '', '']);
+});
+
+// tests winning combinations
+test('it should show a winner if a player wins', () => {
+  expect().toMatch();
+});
+
+// tests for a draw on the board
+test('it should display a draw if no player wins', () => {
+  expect().toBe();
+});
+
+// tests for display of only the winning player
+test('it shoud only display a winner not a loser if a player wins', () => {
+  expect().toBe();
+});
+
+// tests for turns of play
+test('it should allow player only one play on each turn', () => {
+  expect().toBe();
+});
+
+// tests for cleaning the board
+test('it should clear the board during a restart', () => {
+  expect().toBe();
 });
